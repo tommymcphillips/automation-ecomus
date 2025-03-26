50a12880f214029c5b4f88022b5513ae5c29c6c1
Thomas McPhillips <t.b.mcphillips.q@gmail.com>
2025-03-26 19:27:16 -0300

    first commit, first page object model home

---
+++ pages/home.page.ts
@@ -0,0 +1,22 @@
+import { Page, Locator } from '@playwright/test';
+
+export class HomePage {
+  readonly page: Page;
+  readonly profileIcon: Locator;
+
+
+  constructor(page: Page) {
+    this.page = page;
+    this.profileIcon = page.locator("ul > li.nav-account > a")
+  }
+
+  async clickProfileIcon() {
+    await this.profileIcon.click();
+    this.page.locator('cualquierCosaa')
+  }
+
+  async goto() {
+    await this.page.goto('https://playwright.dev/');
+  }
+
+}
