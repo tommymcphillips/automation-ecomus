77280bc3006ec1daf0b7ec159c40a479a40e18d2
Thomas McPhillips <t.b.mcphillips.q@gmail.com>
2025-03-26 19:37:24 -0300

    removed the slash in the end the url

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -16,7 +16,7 @@ export class HomePage {
   }
 
   async goto() {
-    await this.page.goto('https://automation-portal-bootcamp.vercel.app/');
+    await this.page.goto('https://automation-portal-bootcamp.vercel.app');
   }
 
 }
